7bfda5378e85d98448bc4e1419af3b15d42b17ee
RubabaSamadzada <rsmdzad23884@ada.edu.az>
2026-02-26 11:09:40 +0400

    Add files via upload

---
+++ MathDemo.java
@@ -0,0 +1,28 @@
+package week06;
+
+public class MathDemo {
+
+    public static int min(int a, int b) {
+        if (a < b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    public static int max(int a, int b) {
+         if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    public static int sum(int[] args) {
+        int total = 0;
+        for (int i = 0; i < args.length; i++) {
+            total += args[i];
+        }
+        return total;
+    }
+}
